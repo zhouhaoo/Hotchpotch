f248b50b44b998fc11a2de6a7cfa42a4057081ba
zhouhaoo <zhouhao7@icloud.com>
2023-04-08 18:30:22 +0800

    融合Flutter

--- android/app/src/main/java/com/zhouhaoh/hotchpotch/sample/ui/MainActivity.kt
+++ android/app/src/main/java/com/zhouhaoh/hotchpotch/sample/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package com.zhouhaoh.hotchpotch.sample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.zhouhaoh.hotchpotch.sample.R
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             println("Assets目录测试结果来源1：$readLine")
             println("Assets目录测试结果来源2：$readLine2")
         } catch (e: Exception) {
+        }
+        findViewById<Button>(R.id.bt_flutter).setOnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            )
         }
     }
 }
--- flutter_module/README.md
+++ flutter_module/README.md
@@ -9,3 +9,9 @@ For help getting started with Flutter development, view the online
 
 For instructions integrating Flutter modules to your existing applications,
 see the [add-to-app documentation](https://flutter.dev/docs/development/add-to-app).
+
+## 报错提示
+
+> Caused by: org.gradle.api.InvalidUserCodeException: Build was configured to prefer settings repositories over project repositories but repository 'maven' was added by plugin class 'FlutterPlugin'
+
+https://lixiaopeng.top/article/92.html
